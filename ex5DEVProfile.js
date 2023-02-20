d536243da222259491076836a2cbc523e60739a9
Karine Lucietti <karine_lucietti@estudante.sc.senai.br>
2023-02-20 20:26:56 -0300

    first commit

---
+++ ex5DEVProfile.js
@@ -0,0 +1,24 @@
+const apiUrl = 'https://api.github.com/users/KarineLucietti';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const profileCard = document.querySelector('.card');
+    const avatar = profileCard.querySelector('.avatar_url');
+    const name = profileCard.querySelector('.name');
+    const login = profileCard.querySelector('.login');
+    const bio = profileCard.querySelector('.bio');
+    const location = profileCard.querySelector('.location');
+/*     const followers = profileCard.querySelector('.followers');
+    const following = profileCard.querySelector('.following'); */
+    const repos = profileCard.querySelector('.public_repos');
+
+    avatar.src = data.avatar_url;
+    name.textContent = data.name;
+    bio.textContent = data.bio;
+    login.textContent = "@" + data.login;
+    location.textContent = data.location;
+/*     followers.textContent = data.followers;
+    following.textContent = data.following; */
+    repos.textContent = data.public_repos;
+  });
